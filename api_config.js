11f92d0b755f9d293146a684d9330d2f44c391ac
chalekuma-rgb <chalekuma-rgb@users.noreply.github.com>
2026-08-30 03:36:12 +0000

    deploy: c6ca8c2f63aedb17fbba19487aa54d652964ab8e

--- api_config.js
+++
@@ -1,3 +0,0 @@
-// Set the API_BASE_URL for the Flutter web app at runtime.
-// This should match the deployed backend endpoint used by the production build.
-window.API_BASE_URL = 'https://qkvroehsycfvrlfclskp.supabase.co/rest/v1';
--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -35,17 +35,8 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7bf","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-
-const loadingElement = document.getElementById('loading');
-
 _flutter.loader.load({
-  onEntrypointLoaded: async function (engineInitializer) {
-    const appRunner = await engineInitializer.initializeEngine();
-
-    if (loadingElement) {
-      loadingElement.remove();
-    }
-
-    await appRunner.runApp();
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "2157585629" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
